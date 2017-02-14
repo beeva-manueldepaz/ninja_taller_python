3f4ba44ec7d6410e77e00196955ac0903ced2e74
meusebio.depaz <meusebio.depaz@bbva.com>
2017-02-14 13:12:07 +0100

    Testing

--- 02_web/tests/test_api.py
+++ 02_web/tests/test_api.py
@@ -10,4 +10,22 @@ def test_api_home():
 
 def test_api_json():
     r = requests.get("http://127.0.0.1:5000/hello")
-    assert r.content == json.dumps(dict(message="Hello from python"))
+    # assert r.content == json.dumps(dict(message="Hello from python")) # problemas de formato
+
+    r_json = json.loads(r.content.decode())
+    value = dict(message="Hello from python")
+    assert r_json == value
+
+def test_api_sum_ko():
+    r = requests.get("http://127.0.0.1:5000/sum")
+
+    r_json = json.loads(r.content.decode())
+    value = dict(sum=0)
+    assert r_json == value
+
+def test_api_sum_ok():
+    r = requests.get("http://127.0.0.1:5000/sum?num1=2&num2=2")
+
+    r_json = json.loads(r.content.decode())
+    value = dict(sum=4)
+    assert r_json == value
